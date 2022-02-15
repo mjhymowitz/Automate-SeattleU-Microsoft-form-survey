9bed8848b0ea58b7696bbf9a86dfc7ccce9fbb9e
mhymowitz <mark.hymowitz@gmail.com>
2022-02-15 00:00:31 -0800

    securing variables and credentials

--- AutomateMicrosoftForm.py
+++ AutomateMicrosoftForm.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+from config import *
 from selenium.webdriver import Edge
 import time
 
 # variables
-EMAIL = '' 	# Enter your SU email here
-PASSWORD = '' 	# Enter your SU password here
 SEATTLEU_COVID = "https://www.seattleu.edu/coronavirus/screening/"
 
 #AUTOMATION
@@ -17,9 +15,9 @@ try:
     # Open School Website
     driver.get(SEATTLEU_COVID)
     form_url = driver.find_element_by_xpath('//*[@id="id1647669"]/div/div/ul/li[1]/a') 
-    time.sleep(2)
+    time.sleep(short_sleep)
     driver.get(form_url.get_attribute('href'))
-    time.sleep(8)
+    time.sleep(long_sleep)
 
     # Enter Username
     user = driver.find_element_by_xpath('//*[@id="i0116"]')
@@ -28,7 +26,7 @@ try:
     # Submit Username
     submit_user = driver.find_element_by_xpath('//*[@id="idSIButton9"]')
     submit_user.click()
-    time.sleep(2)
+    time.sleep(short_sleep)
 
     # Enter Password
     user = driver.find_element_by_xpath('//*[@id="i0118"]')
@@ -37,12 +35,12 @@ try:
     # Submit Password
     submit_user = driver.find_element_by_xpath('//*[@id="idSIButton9"]')
     submit_user.click()
-    time.sleep(2)
+    time.sleep(short_sleep)
 
     # Stay signed in?
     submit_user = driver.find_element_by_xpath('//*[@id="idBtn_Back"]')
     submit_user.click()
-    time.sleep(2)
+    time.sleep(short_sleep)
 
     # Filling out form
     RadioButtonPeriod = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/div/label/input')
@@ -66,7 +64,7 @@ try:
     # Stay signed in?
     submit_user = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[3]/div[1]/button/div')
     submit_user.click()
-    time.sleep(2)
+    time.sleep(short_sleep)
 
     # Confirm if code worked or not
     get_confirmation_div_text = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[2]/div[1]/div[2]/span')
---
+++ config-Template.py
@@ -0,0 +1,9 @@
+# rename this file to config.py and enter your username and password below
+# before executing code
+
+EMAIL = '' 	# Enter your SU email here
+PASSWORD = '' 	# Enter your SU password here
+
+#these variables will need to be adjusted to fit your internet speed
+long_sleep = 8              
+short_sleep = 2
